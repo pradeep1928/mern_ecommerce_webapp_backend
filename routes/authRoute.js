2b0f50015d1832dbd23ce111f42d9997b6e4acab
Pradeep Padmukhi <ppadmukhi28@gmail.com>
2022-04-02 21:37:47 +0530

    created routes

---
+++ routes/authRoute.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+
+const User = require("../models/user");
+
+//Register route
+router.post('/register', async (req, res) => {
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    try {
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser)
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
+
+
+
+
+
+module.exports = router;
