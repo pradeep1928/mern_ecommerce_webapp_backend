4e94eba508d4c8533949651e6a4bbb8a504f4882
Pradeep Padmukhi <ppadmukhi28@gmail.com>
2022-04-02 21:34:44 +0530

    created server using express

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+const userRoute = require("./routes/userRoute");
+const authRoute = require("./routes/auth");
+
+
+// configuration
+const app = express();
+dotenv.config();
+app.use(express.json());
+
+
+// mongoDB connection
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connection successfull"))
+  .catch((error) => {
+    console.log(error);
+  });
+
+
+  // Routes 
+  app.use("/api/users", userRoute);
+  app.use("/api/auth", authRoute);
+
+// Listening to port
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
