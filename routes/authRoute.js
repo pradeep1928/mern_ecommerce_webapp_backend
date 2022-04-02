6755fe9aa4e7b60d7eac436339d7c74f15928721
Pradeep Padmukhi <ppadmukhi28@gmail.com>
2022-04-03 01:16:41 +0530

    created login route and password hashed with crypto-js

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -5,23 +5,50 @@ const CryptoJs = require("crypto-js");
 const User = require("../models/user");
 
 //Register route
-router.post('/register', async (req, res) => {
-    const newUser = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: CryptoJs.AES.encrypt(req.body.password, process.env.PASS_SEC).toString() 
-    });
+router.post("/register", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJs.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString(),
+  });
 
-    try {
-        const savedUser = await newUser.save();
-        res.status(201).json(savedUser)
-    } catch (error) {
-        res.status(500).json(error);
-    }
-})
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
+//Login route
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+ 
+    // checking if not user
+    // use of && is similar to use of if condition
+    !user && res.status(401).json("Wrong username or password");
 
+    // decrypting password
+    const hashedPassword = CryptoJs.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    );
+    const originalPassword = hashedPassword.toString(CryptoJs.enc.Utf8);
 
+    originalPassword != req.body.password &&
+      res.status(401).json("Wrong username or password");
 
+    // destructuring password and other information of user
+    // and sending only other information, and not password
+    const { password, ...otherInfo } = user._doc;
+    res.status(200).json(otherInfo);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 module.exports = router;
