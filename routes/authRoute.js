cdd4492b8d532f4a07866679734a4d099f3f5444
Pradeep Padmukhi <ppadmukhi28@gmail.com>
2022-04-02 21:45:09 +0530

    crypto-js added to encrypt password

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const CryptoJs = require("crypto-js");
 
 const User = require("../models/user");
 
@@ -8,7 +9,7 @@ router.post('/register', async (req, res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password
+        password: CryptoJs.AES.encrypt(req.body.password, process.env.PASS_SEC).toString() 
     });
 
     try {
