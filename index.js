8a0ab44fb12f6c3294c01431fccbc60de2438456
Pradeep Padmukhi <ppadmukhi28@gmail.com>
2022-04-04 20:58:59 +0530

    added stripe payment method for backend

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 const userRoute = require("./routes/userRoute");
 const authRoute = require("./routes/authRoute");
 const productRoute = require("./routes/productRoute");
 const cartRoute = require("./routes/cartRoute");
 const orderRoute = require("./routes/orderRoute");
-
+const stripeRoute = require("./routes/stripeRoute");
 
 // configuration
 const app = express();
 dotenv.config();
 app.use(express.json());
-
+app.use(cors());
 
 // mongoDB connection
 mongoose
@@ -26,13 +27,13 @@ mongoose
     console.log(error);
   });
 
-
-  // Routes 
-  app.use("/api/users", userRoute);
-  app.use("/api/auth", authRoute);
-  app.use("/api/products", productRoute);
-  app.use("/api/carts", cartRoute);
-  app.use("/api/orders", orderRoute);
+// Routes
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
 
 // Listening to port
 const port = process.env.PORT || 5000;
